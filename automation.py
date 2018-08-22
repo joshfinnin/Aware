10b06ab5d3a53e2dc0a03ddfe262078e18234c61
Josh.Finnin <joshua.finnin@gmail.com>
2018-08-22 18:19:21 +1000

    Module for performing automated updates of drawings. Currently only contains a TODO list

---
+++ automation.py
@@ -0,0 +1,7 @@
+"""Module for automated updates of the Current (Issued) folder based on previously defined settings."""
+
+# TODO: Load project folders from ProgramData folder
+# TODO: Find settings files to load settings from
+# TODO: Create DisciplineSettings objects from loaded settings
+# TODO: Perform drawing update based on DisciplineSettings objects
+
